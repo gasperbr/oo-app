d4f5d55b0f5497846b77811c581d6b660c1c4ea4
Tara <tb7412@student.uni-lj.si>
2019-06-02 13:48:27 +0200

    surf-spot finish

--- src/app/components/surf-spot/surf-spot.component.ts
+++ src/app/components/surf-spot/surf-spot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-surf-spot',
@@ -10,6 +11,99 @@ export class SurfSpotComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    var clicks = 0;
+
+    $(document).ready(function() {
+      $(".title-oahu").click(
+        function(){
+          if(clicks == 0){
+            $('.title-zarautz').addClass('hideDiv');
+            $('.title-j-bay').addClass('hideDiv');
+            $('.title-mentawai').addClass('hideDiv');
+            $('.mentawai_beach').addClass('change1');
+            $('.under_water').addClass('changeSmall3');
+            clicks=1;
+          } else {
+            $('.title-j-bay').removeClass('hideDiv');
+            $('.title-mentawai').removeClass('hideDiv');
+            $('.title-zarautz').removeClass('hideDiv');
+            $('.mentawai_beach').removeClass('change1');
+            $('.under_water').removeClass('changeSmall3');
+            clicks = 0;
+          }
+      });
+    });
+
+    $(document).ready(function() {
+      $(".title-mentawai").click(
+        function(){
+          if(clicks == 0){
+            $('.title-zarautz').addClass('hideDiv');
+            $('.title-j-bay').addClass('hideDiv');
+            $('.title-oahu').addClass('hideDiv');
+            $('.under_water').removeClass('showImage');
+            clicks = 1;
+          } else {
+            $('.title-j-bay').removeClass('hideDiv');
+            $('.title-oahu').removeClass('hideDiv');
+            $('.title-zarautz').removeClass('hideDiv');
+            $('.under_water').addClass('showImage');
+            console.log(clicks);
+            clicks = 0;
+          }
+      });
+    });
+
+    $(document).ready(function() {
+      $(".title-zarautz").click(
+        function(){
+          if(clicks == 0){
+            $('.title-oahu').addClass('hideDiv');
+            $('.title-j-bay').addClass('hideDiv');
+            $('.title-mentawai').addClass('hideDiv');
+            $('.mentawai_beach').addClass('change2');
+            $('.under_water').removeClass('showImage');
+            $('.under_water').addClass('changeSmall2');
+
+            clicks = 1;
+          } else {
+            $('.title-j-bay').removeClass('hideDiv');
+            $('.title-mentawai').removeClass('hideDiv');
+            $('.title-oahu').removeClass('hideDiv');
+            $('.mentawai_beach').removeClass('change2');
+            $('.under_water').addClass('showImage');
+            $('.under_water').removeClass('changeSmall2');
+
+            clicks = 0;
+          }
+      });
+    });
+
+    $(document).ready(function() {
+      $(".title-j-bay").click(
+        function(){
+          if(clicks == 0){
+            $('.title-zarautz').addClass('hideDiv');
+            $('.title-oahu').addClass('hideDiv');
+            $('.title-mentawai').addClass('hideDiv');
+            $('.under_water').addClass('changeSmall4');
+            $('.under_water').removeClass('showImage');
+            $('.mentawai_beach').addClass('change3');
+            clicks = 1;
+          } else {
+            $('.title-oahu').removeClass('hideDiv');
+            $('.title-mentawai').removeClass('hideDiv');
+            $('.title-zarautz').removeClass('hideDiv');
+            $('.under_water').addClass('showImage');
+            $('.under_water').removeClass('changeSmall4');
+            $('.mentawai_beach').removeClass('change3');
+            clicks = 0;
+          }
+      });
+    });
+
   }
+
 
 }
