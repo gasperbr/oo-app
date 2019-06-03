503888b93eba3b12f1e48ea7568d4cf8f77af3d3
gasper <cybergrid@proxima.si>
2019-06-03 10:21:40 +0200

    change landing

--- src/app/components/video-section/video-section.component.ts
+++ src/app/components/video-section/video-section.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterContentInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-video-section',
   templateUrl: './video-section.component.html',
   styleUrls: ['./video-section.component.scss']
 })
-export class VideoSectionComponent implements OnInit {
+export class VideoSectionComponent implements OnInit, AfterContentInit {
 
+/*   @ViewChild('myVideo') video: ElementRef
+
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const position = this.video.nativeElement.getBoundingClientRect();
+    console.log(position.top, window.pageYOffset);
+  }
+ */
   constructor() { }
 
+  ngAfterContentInit() {
+  }
+
+/*   play() {
+    this.video.nativeElement.play();
+  }
+ */
   ngOnInit() {
   }
 
